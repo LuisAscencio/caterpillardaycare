879f239ac4336923717ea9f747f2e69ba011466e
Luis Ascencio <luismiguelascencio@gmail.com>
2020-03-25 14:08:25 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 import "./App.css";
 
 function App() {
