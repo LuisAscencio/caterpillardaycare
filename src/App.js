04146ef79dab1f74c9d359d514cf2225d4cef2a5
Luis Ascencio <luismiguelascencio@gmail.com>
2020-03-26 21:42:21 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,43 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import "./App.css";
 
+import PhotoRoll from "./components/PhotoRoll";
+
 function App() {
-  return <div>Caterpillar Daycare</div>;
+  return (
+    <div className=".all">
+      <Navbar sticky="top" variant="dark" bg="dark" expand="lg">
+        <Navbar.Brand>
+          <div>Caterpillar Daycare</div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Dashboard</Nav.Link>
+            <Nav.Link href="/visitsvault"> Visits vault</Nav.Link>
+            <Nav.Link href="/newvisit">New visit</Nav.Link>
+
+            {/* <Nav.Link href="/login">
+              <FaPowerOff size="20px" />
+            </Nav.Link> */}
+            {/* <Nav.Link href="/signup">Sign up</Nav.Link> */}
+
+            {/* <Button
+          onClick={() => {
+            app.auth().signOut();
+            // localStorage.clear();
+            console.log(localStorage);
+          }}
+        >
+          Sign Out
+        </Button> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <PhotoRoll />
+    </div>
+  );
 }
 
 export default App;
