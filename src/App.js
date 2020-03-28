7bb8a76587d16212e16ed484369580c3389817f3
Luis Ascencio <luismiguelascencio@gmail.com>
2020-03-28 17:14:15 -0400

    about us

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import "./App.css";
+import { AboutUs } from "./components/AboutUs";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      {/* <NavBar /> */}
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+
+export const AboutUs = () => {
+  return (
+    <div>
+      <Container>
+        <h1>Hello Guys</h1>
+      </Container>
+    </div>
+  );
+};
