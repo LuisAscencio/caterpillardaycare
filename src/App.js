7e4e17614a0e20adef6c381ed30ce351f61eccbe
Luis Ascencio <luismiguelascencio@gmail.com>
2020-03-31 16:13:45 -0400

    testimonials comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Testimonials from "./components/Testimonials";
 import "./App.css";
 import { AboutUs } from "./components/AboutUs";
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <NavBar />
       <AboutUs />
+      <Testimonials />
     </div>
   );
 }
