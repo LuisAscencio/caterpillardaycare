27847c24db7e818a330bb1c232985da31e522341
Luis Ascencio <luismiguelascencio@gmail.com>
2020-04-01 14:42:35 -0400

    gallery component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Testimonials from "./components/Testimonials";
+import Gallery from "./components/Gallery";
 import "./App.css";
 import { AboutUs } from "./components/AboutUs";
 
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <AboutUs />
       <Testimonials />
+      <Gallery />
     </div>
   );
 }
